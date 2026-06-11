189eef4e260850e78d5870153461ff1b48e3cdb1
Aliya Alisaii <AliyaRashidSarhan@gmail.com>
2026-06-11 09:57:27 +0400

    add Context in main program

--- Hospital Management System/Hospital Management System/Program.cs
+++ Hospital Management System/Hospital Management System/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Hospital_Management_System
+﻿using Hospital_Management_System.Module;
+
+namespace Hospital_Management_System
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            HospitalContext mainContext = new HospitalContext();
+            mainContext.Patients = new List<Patient>();
+            mainContext.Doctors= new List<Doctor>();
+            mainContext.Appointments= new List<Appointment>();
+            mainContext.AvailableSlots = new List<AvailableSlot>();
+            mainContext.MedicalRecords = new List<MedicalRecord>();
+
+
+         
         }
     }
 }
